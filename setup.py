9389ecb3436e19988f935621eef230cddfadc600
Kenneth Reitz <me@kennethreitz.com>
2012-05-08 01:27:05 -0400

    args

---
+++ setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+args
+~~~~
+
+This simple module gives you an elegant interface for your command line argumemnts.
+
+"""
+
+from setuptools import setup
+
+setup(
+    name='dj-database-url',
+    version='1.0.0',
+    url='https://github.com/kennethreitz/args',
+    license='BSD',
+    author='Kenneth Reitz',
+    author_email='me@kennethreitz.com',
+    description='Command Arguments for Humans.',
+    long_description=__doc__,
+    py_modules=['args'],
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
