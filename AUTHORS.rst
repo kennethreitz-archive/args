18d22c28b82e9bbc8f22ccada575fb92fa851aac
kracekumar <kracethekingmaker@gmail.com>
2012-05-09 21:35:41 +0530

    1. Added Test\n2. Added requirements.txt\n 3. Added .travis.yml\n 4.Added AUTHORS.rst

---
+++ AUTHORS.rst
@@ -0,0 +1,12 @@
+Args is written and maintained by Kenneth Reitz and various contributors.
+
+Development Lead
+--------
+    - Kenneth Reitz <me@kennethreitz.com>
+
+Patches and Suggestions
+---
+    - Kracekumar <me@kracekumar.com>
+
+
+
---
+++ tests.py
@@ -0,0 +1,55 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import args
+from nose.tools import ok_
+
+def test_args_all():
+    arguments = ['install', '--lang', 'python', 'c', 'js']
+    arg = args.ArgsList(args = arguments)
+    ok_(arg.all == arguments)
+
+def test_flags():
+    flags = ['--name', '--email']
+    arguments = [flags[0], 'kracekumar', flags[1], 'me@kracekumar']
+    arg = args.ArgsList(args = arguments)
+    ok_(arg.flags.all == flags)
+
+
+def test_files():
+    files = ['*.py']
+    arg = args.ArgsList(args = files)
+    #any way current directory will have minimum one file i.e this file
+    ok_(len(arg.files) > 1)
+
+def test_not_files():
+    flags = ['--name', '--email']
+    arguments = [flags[0], 'kracekumar', flags[1], 'me@kracekumar', '*.py']
+    arg = args.ArgsList(args = arguments)
+    arguments.pop()
+    ok_(arg.not_files.all == arguments)
+
+def test_grouped():
+    details = {'--language': ['python'], '--creator': ['Guido Van Rossum'], \
+               '--foundation': ['psf']
+               }
+    arguments = []
+    for key in details:
+        arguments.append(key)
+        arguments.append(details[key][0])
+
+    arg = args.ArgsList(args = arguments)
+    for item in arg.grouped:
+        if item is not '_':
+            ok_(arg.grouped[item].all == details[item])
+
+def test_start_with():
+    dynamic_lang = ['python', 'perl']
+    static_lang = ['c', 'c++']
+    arguments = ['--lang']
+    arguments.extend(dynamic_lang)
+    arguments.extend(static_lang)
+    arg = args.ArgsList(args = arguments)
+    ok_(arg.start_with('p').all == dynamic_lang)
+
+
